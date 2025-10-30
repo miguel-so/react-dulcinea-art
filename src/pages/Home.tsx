0b3bb94cf76981fb9c10c8ccc652cc08f15f015a
buddy1014 <59859330+buddy1014@users.noreply.github.com>
2025-10-30 16:16:49 -0400

    Merge pull request #1 from miguel-so/feat_home-page_categories
    style: fix style of art gallery swiper buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,10 @@
 // pages/Home.tsx
 import { Box } from "@chakra-ui/react";
+
 import { Hero } from "../components/home-page/Hero";
 import { ArtGallery } from "../components/home-page/ArtGallery";
 import { Categories } from "../components/home-page/Categories";
 import { ArtistSpotlight } from "../components/home-page/ArtistSpotlight";
-
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
-import "./Home.css";
 
 const Home = () => {
   return (
